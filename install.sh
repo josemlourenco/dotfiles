13faf9b5132f24368dc0da8140134fd691ac7ce3
José Maria Belo De Oliveira Da Silva Lourenço <jose.lourenco@hibob.io>
2025-02-27 17:00:13 +0000

    remove all

--- install.sh
+++ install.sh
@@ -1,10 +1,2 @@
 #!/usr/bin/env bash
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-brew install neovim
-
-git clone https://github.com/LazyVim/starter ~/.config/nvim
-rm -rf ~/.config/nvim/.git
-
-nivm
