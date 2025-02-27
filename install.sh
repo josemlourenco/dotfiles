15f251c5e79fe16fb86e2adfa4bdada383df6b6d
José Maria Belo De Oliveira Da Silva Lourenço <jose.lourenco@hibob.io>
2025-02-27 17:23:17 +0000

    add nerd font

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 brew install neovim
 
-git clone https://github.com/LazyVim/starter ~/.config/nvim
+git clone --depth 1 https://github.com/LazyVim/starter ~/.config/nvim && nvim --headless -c 'autocmd User LazyVimEnter silent! lua require("lazy").sync(); quit'
 rm -rf ~/.config/nvim/.git
 
+wget -qO- https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip | unzip -q - && mkdir -p ~/.fonts && mv "JetBrainsMonoNerdFont-Regular.ttf" "JetBrainsMonoNerdFont-Bold.ttf" "JetBrainsMonoNerdFont-Italic.ttf" "JetBrainsMonoNerdFont-BoldItalic.ttf" ~/.fonts/ && fc-cache -fv && gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$(gsettings get org.gnome.Terminal.Legacy.Profile:/default | tr -d \')/ font 'JetBrainsMono Nerd Font Mono 12' && rm JetBrainsMono.zip "JetBrainsMonoNerdFont-Regular.ttf" "JetBrainsMonoNerdFont-Bold.ttf" "JetBrainsMonoNerdFont-Italic.ttf" "JetBrainsMonoNerdFont-BoldItalic.ttf"
